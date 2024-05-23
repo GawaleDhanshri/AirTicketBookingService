887276e94e59a45db311a72ef4ab9c05e1533b7d
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-05-23 18:06:01 +0530

    updated some changes

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,41 +1,41 @@
-const { StatusCodes } = require('http-status-codes');
 
-const { Booking } = require('../models/index');
-const { AppError, ValidationError } = require('../utils/errors/index');
+const axios = require('axios');
 
-class BookingRepository {
-    async create(data) {
-        try {
-            const booking = await Booking.create(data);
-            return booking;
-        } catch (error) {
-            if(error.name == 'SequelizeValidationError') {
-                throw new ValidationError(error);
-            }
-            throw new AppError(
-                'RepositoryError', 
-                'Cannot create Booking', 
-                'There was some issue creating the booking, please try again later',
-                StatusCodes.INTERNAL_SERVER_ERROR);
-        }
+const { BookingRepository } = require('../repository/index');
+const { FLIGHT_SERVICE_PATH } = require('../config/serverConfig');
+const { ServiceError } = require('../utils/errors');
+
+class BookingService {
+    constructor() {
+        this.bookingRepository = new BookingRepository();
     }
 
-    async update(bookingId, data) {
+    async createBooking(data) {
         try {
-            const booking = await Booking.findByPk(bookingId);
-            if(data.status) {
-                booking.status = data.status;
+            const flightId = data.flightId;
+            const getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+            const response = await axios.get(getFlightRequestURL);
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if(data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
             }
-            await booking.save();
-            return booking;
-        } catch (error) {
-            throw new AppError(
-                'RepositoryError', 
-                'Cannot update Booking', 
-                'There was some issue updating the booking, please try again later',
-                StatusCodes.INTERNAL_SERVER_ERROR);
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data, totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            console.log(updateFlightRequestURL);
+            await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
+            return finalBooking;
+        } catch (error) { 
+            console.log(error);
+            if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
+                throw error;
+            }
+            throw new ServiceError();
         }
     }
 }
 
-module.exports = BookingRepository;
+module.exports = BookingService;
